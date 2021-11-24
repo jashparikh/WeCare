275ae604d0383c3a9e67c95fea09f58ca45e8e91
aditya <aditya@10.66.0.106>
2021-11-24 18:05:16 +0000

    added generate client schedule in controller

--- wecare/src/main/java/com/springboot/wecare/controller/ClientScheduleController.java
+++ wecare/src/main/java/com/springboot/wecare/controller/ClientScheduleController.java
@@ -3,9 +3,11 @@ package com.springboot.wecare.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.wecare.model.Appointment;
 import com.springboot.wecare.model.Caregiver;
 import com.springboot.wecare.service.IClientScheduleService;
 
@@ -15,11 +17,18 @@ public class ClientScheduleController {
 	@Autowired
 	IClientScheduleService clientScheduleService;
 	
+	@CrossOrigin
 	@GetMapping("/findClientSchedule")
 	public List<Long> findClientSchedule(Long clientId) {
 		
 
 		return clientScheduleService.getClientSchedule(clientId);
+	}
+	
+	@CrossOrigin
+	@GetMapping("/generateClientSchedule")
+	public List<Appointment> generateClientSchedule(long firstAptId){
+		return clientScheduleService.generateClientSchedule(firstAptId);
 	}
 
 }
