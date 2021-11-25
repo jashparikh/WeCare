33c9e28ab44b886d603163bb3a057f5c14c7f338
aditya <aditya@10.66.0.106>
2021-11-25 14:05:25 +0000

    Fixed issue in First Time Appointment Controller

--- wecare/src/main/java/com/springboot/wecare/controller/FirstTimeAppointmentController.java
+++ wecare/src/main/java/com/springboot/wecare/controller/FirstTimeAppointmentController.java
@@ -6,10 +6,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.wecare.model.FirstTimeAppointment;
@@ -38,9 +40,8 @@ public class FirstTimeAppointmentController {
 	}
 
 	@CrossOrigin
-	@PutMapping("/cancelFirstAppointment")
-
-	public String cancelFirstAppointment(@RequestBody @Valid Long firsttimeappointmentId) {
+	@DeleteMapping("/cancelFirstAppointment")
+    public String cancelFirstAppointment(@RequestParam @Valid Long firsttimeappointmentId) {
 		String response = firstTimeService.cancelFirstAppointment(firsttimeappointmentId);
 
 		return response;
